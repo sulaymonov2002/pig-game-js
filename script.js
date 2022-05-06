250839370791bc2cc5875a9ae491278287a5edfe
sulaymonov2002 <ssamir.web2002@gmail.com>
2022-05-06 15:29:39 +0500

    ends of pig-game-v2

---
+++ script.js
@@ -0,0 +1,80 @@
+"use strict";
+
+const player0El = document.querySelector(".player--0");
+const player1El = document.querySelector(".player--1");
+const score0El = document.getElementById("score--0");
+const score1El = document.getElementById("score--1");
+const current0El = document.getElementById("current--0");
+const current1El = document.getElementById("current--1");
+const diceEl = document.querySelector(".dice");
+const btnNew = document.querySelector(".btn--new");
+const btnRoll = document.querySelector(".btn--roll");
+const btnHold = document.querySelector(".btn--hold");
+
+let scores, currentscore, activeplayer, playing;
+const newGame = function () {
+  scores = [0, 0];
+  currentscore = 0;
+  activeplayer = 0;
+  playing = true;
+
+  score0El.textContent = 0;
+  score1El.textContent = 0;
+  current0El.textContent = 0;
+  current1El.textContent = 0;
+
+  diceEl.classList.add("hidden");
+  player0El.classList.remove("player--winner");
+  player1El.classList.remove("player--winner");
+  player0El.classList.add("player--active");
+  player1El.classList.remove("palyer--active");
+  document.querySelector("body").style.backgroundColor = "#c7365f";
+};
+newGame();
+
+const switchPlayer = function () {
+  document.getElementById(`current--${activeplayer}`).textContent = 0;
+  currentscore = 0;
+  activeplayer = activeplayer === 0 ? 1 : 0;
+  player0El.classList.toggle("player--active");
+  player1El.classList.toggle("player--active");
+};
+
+btnRoll.addEventListener("click", function () {
+  if (playing) {
+    const dice = Math.trunc(Math.random() * 6) + 1;
+    diceEl.classList.remove("hidden");
+    diceEl.src = `dice-${dice}.png`;
+    if (dice !== 1) {
+      currentscore += dice;
+      document.getElementById(`current--${activeplayer}`).textContent =
+        currentscore;
+    } else {
+      switchPlayer;
+    }
+  }
+});
+
+btnHold.addEventListener("click", function () {
+  if (playing) {
+    scores[activeplayer] += currentscore;
+    document.getElementById(`score--${activeplayer}`).textContent =
+      scores[activeplayer];
+      if (scores[activeplayer >= 20]) {
+          playing = false;
+      diceEl.classList.add("hidden");
+      document
+        .querySelector(`.player--${activeplayer}`)
+        .classList.add("player--active");
+      document
+        .querySelector(`.player--${activeplayer}`)
+        .classList.remove("player--active");
+      document.querySelector("body").style.backgroundColor = "#64b347";
+    } else {
+      switchPlayer();
+    }
+  }
+});
+
+
+btnNew.addEventListener('click', newGame);
